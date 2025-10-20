02b01c34b205148bb91935cd47a73555bf2b6e6d
Riteesh17 <riteeshriteesh1736736@gmail.com>
2025-10-20 17:54:32 +0530

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,10 +39,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //production script
-app.use(express.static("./client/build"));
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-});
+// app.use(express.static("./client/build"));
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://mern-vcc-azure-web-app-gscccqgtddembbfh.southeastasia-01.azurewebsites.net/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
