2cee56224541702eb59a8724c8756c415389be95
Riteesh17 <riteeshriteesh1736736@gmail.com>
2025-10-20 14:19:05 +0530

    Azure deploy(static,url)

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
+//const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -36,6 +37,12 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
+//production script
+app.use(express.static("./client/build"));
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
